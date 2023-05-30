f669b69e5ab8b98494aa971f824300e03c25a160
Jure <jure.djakovic@student.um.si>
2023-05-30 10:40:53 +0200

    Initial commit

---
+++ android/src/main/java/com/ktx/planes/android/AndroidLauncher.kt
@@ -0,0 +1,16 @@
+package com.ktx.planes.android
+
+import android.os.Bundle
+import com.badlogic.gdx.backends.android.AndroidApplication
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
+import com.ktx.planes.Planes
+
+/** Launches the Android application.  */
+class AndroidLauncher : AndroidApplication() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val configuration = AndroidApplicationConfiguration()
+        configuration.useImmersiveMode = true // Recommended, but not required.
+        initialize(Planes(), configuration)
+    }
+}
